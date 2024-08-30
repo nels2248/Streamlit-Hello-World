3b3b0df280c665afcc11870799cfbb3b5d9d3cc6
nels2248 <nels2248@gmail.com>
2024-08-29 21:15:01 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("My First Streamlit App")
 
-name = st.text_input("Enter your name: dude")
+name = st.text_input("Enter your name: dude #2")
 if st.button("Submit"):
     st.write(f"Hello, {name}!")
